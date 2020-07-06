d88a3be636a52af798dbbf9d0486730e5ad28fba
liuyu <beiying_7723@126.com>
2020-07-06 23:20:25 +0800

    update

---
+++ ffplayer/src/main/java/com/beiying/ffplayer/VideoView.java
@@ -0,0 +1,42 @@
+package com.beiying.ffplayer;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.view.Surface;
+import android.view.SurfaceView;
+
+public class VideoView extends SurfaceView {
+    static {
+        System.loadLibrary("avplayer");
+    }
+
+    Surface mSurface;
+    public VideoView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        getHolder().setFormat(PixelFormat.RGBA_8888);
+        mSurface = getHolder().getSurface();
+    }
+
+    public void playVideo(final String videoPath) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                playVideo(videoPath, mSurface);
+            }
+        }).start();
+    }
+
+    public static native String urlProtocolInfo();
+
+    public static native String avFormatInfo();
+
+    public static native String avCodecInfo();
+
+    public static native String avFilterInfo();
+
+    public static native void playVideo(String videoPath, Surface surface);
+}
