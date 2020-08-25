4c30b910252f8a0f7129813b1f84e8842ead3487
liuyu <beiying_7723@126.com>
2020-08-25 23:13:02 +0800

    update

--- bymedia/src/main/java/com/beiying/media/opengl/BaseShapeView.kt
+++ bymedia/src/main/java/com/beiying/media/opengl/BaseShapeView.kt
@@ -7,8 +7,11 @@ import com.beiying.media.opengl.render.BaseRender
 
 class BaseShapeView(context: Context,  val baseRender: BaseRender) : GLSurfaceView(context) {
     init {
+        //设置OpenGL版本
         setEGLContextClientVersion(2)
         setRenderer(baseRender)
+
+        //设置渲染模式，包括实时渲染（每个16ms渲染一次）和按需渲染（需要渲染时才渲染，效率高）
         renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
     }
 
--- bymedia/src/main/java/com/beiying/media/opengl/render/BaseRender.kt
+++ bymedia/src/main/java/com/beiying/media/opengl/render/BaseRender.kt
@@ -1,6 +1,7 @@
 package com.beiying.media.opengl.render
 
 import android.content.Context
+import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -17,12 +18,17 @@ open class BaseRender(val context: Context) : GLSurfaceView.Renderer {
 
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        //Surface创建的时候重置画布为黑色
+        GLES20.glClearColor(0f,0f,0f,0f)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
     }
 
     override fun onDrawFrame(gl: GL10?) {
+        //清空之前的渲染
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT and GLES20.GL_DEPTH_BUFFER_BIT)
+
     }
 
     open fun onSurfaceDestroyed() {
