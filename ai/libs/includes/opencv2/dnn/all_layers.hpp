230974024b427f846428a2d8614284cd35d5d443
liuyu <beiying_7723@126.com>
2020-10-23 18:03:55 +0800

    使用OpenCV4

--- ai/libs/includes/opencv2/dnn/all_layers.hpp
+++ ai/libs/includes/opencv2/dnn/all_layers.hpp
@@ -45,7 +45,7 @@
 
 namespace cv {
 namespace dnn {
-CV__DNN_EXPERIMENTAL_NS_BEGIN
+CV__DNN_INLINE_NS_BEGIN
 //! @addtogroup dnn
 //! @{
 
@@ -595,6 +595,8 @@ CV__DNN_EXPERIMENTAL_NS_BEGIN
     class CV_EXPORTS RegionLayer : public Layer
     {
     public:
+        float nmsThreshold;
+
         static Ptr<RegionLayer> create(const LayerParams& params);
     };
 
@@ -682,7 +684,7 @@ CV__DNN_EXPERIMENTAL_NS_BEGIN
 
 //! @}
 //! @}
-CV__DNN_EXPERIMENTAL_NS_END
+CV__DNN_INLINE_NS_END
 }
 }
 #endif
